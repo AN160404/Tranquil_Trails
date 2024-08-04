b42f841ae4a9ea774a7a35b24071be809d868791
an <atianaim40@gmail.com>
2024-08-04 14:07:31 +0530

    Front-end

---
+++ front-end/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Nav from './components/navbar.mjs';
+function App() {
+  return (
+    <>
+    <div >
+      <Nav/>
+    </div>
+    </>
+  );
+}
+
+export default App;

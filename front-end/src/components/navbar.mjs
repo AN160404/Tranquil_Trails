9d72c8f663fcd9a0f319c109b7762cb761b7a7fc
an <atianaim40@gmail.com>
2024-08-04 18:41:46 +0530

    Navbar done

--- front-end/src/components/navbar.mjs
+++ front-end/src/components/navbar.mjs
@@ -1,43 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-const myStyle1={
-  color:'white',
-  backgroundColor:'rgb(250, 83, 111)',
-  height:'50px',
-  position: 'sticky',
-  top:'0px',
-  padding:'5px',  
-}
+
 const myStyle={
-  color:'white',
-  backgroundColor:'rgb(250, 83, 111)',
-  height:'50px',
-  position: 'sticky',
-  top:'0px',
-  padding:'0px',  
+  marginLeft:'30px',
+  marginRight:'100px',
+  fontSize:'30px'
+}
+const myStyle2={
+  marginLeft:'50px',
+  marginRight:'50px'
 }
 
 export default function Nav(props) {
   return (
-    <><nav style={myStyle} className="navbar navbar-expand-lg bg-body-tertiary">
-    <div className="container-fluid" style={myStyle1}>
-      <a className="navbar-brand" href="/">Tranquil Trails</a>
+    <><nav  className="navbar navbar-expand-lg bg-body-tertiary">
+    <div className="container-fluid" >
+
+    <img classname="logo" src="Logo.png" alt="img not found" />
+
+      
+      <a className="navbar-brand" style={myStyle} href="/">Tranquil Trails</a>
+
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/">Home</a>
+          <li className="nav-item" style={myStyle2}>
+            <a className="nav-link active" aria-current="page" href="/">About</a>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/">Features</a>
+          <li className="nav-item" style={myStyle2}>
+            <a className="nav-link" href="/">Search Image</a>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/">Pricing</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+          <li className="nav-item" style={myStyle2}>
+            <a className="nav-link" href="/">Get Reviews</a>
           </li>
         </ul>
       </div>

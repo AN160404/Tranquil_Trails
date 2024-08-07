3feb9386226f4a57be960e52b7ce0176d5bb599c
23arorakriti <kriti23arora@gmail.com>
2024-08-07 20:51:07 +0530

    editions

---
+++ front-end/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>Main Search</h2>
+      {/* Include your main search functionality here */}
+    </div>
+  );
+}
+
+export default About;
---
+++ front-end/src/components/ImageSearch.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ImageSearch() {
+  return (
+    <div>
+      <h2>Image Search</h2>
+      {/* Include your image search functionality here */}
+    </div>
+  );
+}
+
+export default ImageSearch;
